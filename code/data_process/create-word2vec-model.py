0441b07c1b8ce11e207e165aba57c379fdc206ac
Abhishek Gupta <abhigupt@iu.edu>
2017-04-18 00:32:06 -0700

    fix: dev: fix the playbook to run the spark job

--- code/data_process/create-word2vec-model.py
+++ code/data_process/create-word2vec-model.py
@@ -4,7 +4,7 @@ from pyspark.sql import Row
 from pyspark.ml.feature import Tokenizer, RegexTokenizer
 from pyspark.ml.feature import StopWordsRemover
 from pyspark.ml.feature import Word2Vec
-#from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession
 
 import re
 
@@ -20,8 +20,6 @@ config.read('/opt/word2vec/config.properties')
 
 
 # get config data
-data_location = config.get('DataSection', 'data_location')
-model_location = config.get('DataSection', 'model_location')
 spark_master = config.get('SparkSection', 'spark_master')
 spark_executor_memory = config.get('SparkSection', 'spark_executor_memory')
 min_word_count = config.get('ModelSection', 'min_word_count')
@@ -29,21 +27,18 @@ num_iterations = config.get('ModelSection', 'num_iterations')
 vector_size = config.get('ModelSection', 'vector_size')
 debug_flag = config.get('Debug', 'debug')
 
-conf = (SparkConf()
-         .setMaster(spark_master)
-         .setAppName("WikiWord2Vec")
-         .set("spark.executor.memory", spark_executor_memory))
+conf = SparkConf().setAppName("WikiWord2Vec")
 sc = SparkContext(conf = conf)
 
 
 # DataFrame Mechanism
-#spark = SparkSession.builder.master(spark_master) \
-#        .appName("WikiWord2Vec") \
-#        .config("spark.executor.memory", spark_executor_memory) \
-#        .getOrCreate()
+spark = SparkSession.builder.master(spark_master) \
+        .appName("WikiWord2Vec") \
+        .config("spark.executor.memory", spark_executor_memory) \
+        .getOrCreate()
 
 
-inp = sc.textFile(data_location).map(lambda text: re.sub('[^a-zA-Z0-9\n\.]',' ', text))
+inp = sc.textFile(sys.argv[1]).map(lambda text: re.sub('[^a-zA-Z0-9\n\.]',' ', text))
 row = Row("text")
 df = inp.map(row).toDF()
 tokenizer = Tokenizer(inputCol="text", outputCol="words")
@@ -55,10 +50,10 @@ word2vec = Word2Vec(inputCol="words", outputCol="word2vec")
 word2vec.setVectorSize(int(vector_size))
 word2vec.setMinCount(int(min_word_count))
 model = word2vec.fit(filteredDF)
-model.write().save(model_location)
+model.write().save(sys.argv[2])
 
 if debug_flag == 1:
     synonyms = model.findSynonyms('sachin',10)
     synonyms.show()
 
-#spark.stop()
+spark.stop()
--- code/data_process/create-word2vec-model.sh
+++ code/data_process/create-word2vec-model.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-spark_master=$1
-spark-submit --master spark://${spark_master}:7077 /opt/word2vec/code/data_process/create-word2vec-model.py
+spark_master=`hostname -i`
+
+spark-submit --master yarn --deploy-mode client --executor-memory 1g \
+--name wordcount --conf "spark.app.id=word2vec" /opt/word2vec/data_process/create-word2vec-model.py \
+hdfs://${spark_master}:8020/word2vec/crawldb \
+hdfs://${spark_master}:8020/word2vec/model 
