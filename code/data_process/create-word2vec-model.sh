65e6268e486406ac805f55d8b133112e4685bad3
Avadhoot Agasti <avadhoot.agasti@gmail.com>
2017-04-22 18:49:29 -0700

    :fix:usr: execution memory should be set in config

--- code/data_process/create-word2vec-model.sh
+++ code/data_process/create-word2vec-model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 spark_master=`hostname -i`
 
-spark-submit --master yarn --deploy-mode client --executor-memory 1g \
+spark-submit --master yarn --deploy-mode client  \
 --conf "spark.app.id=word2vec" /opt/word2vec/data_process/create-word2vec-model.py \
 hdfs://${spark_master}:8020/word2vec/crawldb \
 hdfs://${spark_master}:8020/word2vec/model \
